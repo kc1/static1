f4069267d654c48778c11a0188a01cfd72498a86
optionhomes11 <optionhomes11@penguin.lxd>
2020-12-05 13:51:20 -0500

    jj

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -13,15 +13,20 @@ exports.handler = async event => {
 
   // EMAIL, ASKING
 
+  console.log(event.body);
   // const email = 'bob123@mailinator.com';
   const email = JSON.parse(event.body).payload.EMAIL
+
+
+  console.log(email);
   // const asking = '70000';
   const asking = JSON.parse(event.body).payload.ASKING
-  console.log(`Recieved a submission: ${email}`)
+  // console.log(`Recieved a submission: ${email}`)
 
   var formData = { 'email': email, 'first_name': '', 'last_name': asking, 'lists[]': '175546' };
   var encoded = Object.entries(formData).map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join("&");
   var endpoint = 'https://api.sendfox.com/contacts/?' + encoded;
+  return;
 
   return fetch(endpoint, {
     method: 'POST',
