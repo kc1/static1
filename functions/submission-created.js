aa3e2ea4beba495443099ce037326228b03ef248
optionhomes11 <optionhomes11@penguin.lxd>
2020-12-06 20:25:05 -0500

    jj

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -50,36 +50,6 @@ exports.handler = async event => {
 
   };
   const data = JSON.stringify(formData);
-  // const data = JSON.stringify({
-  //   todo: 'Buy the milk'
-  // })
-  
-  // const options = {
-  //   hostname: 'whatever.com',
-  //   port: 443,
-  //   path: '/todos',
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Content-Length': data.length
-  //   }
-  // }
-  
-  // const req = https.request(options, res => {
-  //   console.log(`statusCode: ${res.statusCode}`)
-  
-  //   res.on('data', d => {
-  //     process.stdout.write(d)
-  //   })
-  // })
-  
-  // req.on('error', error => {
-  //   console.error(error)
-  // })
-  
-  // req.write(data)
-  // req.end()
-
   console.log(email);
 
   const req = https.request(endpoint, options, (res) => {
