4375eb2081d82acd5593aa1fd4d92a2a6a3c0548
optionhomes11 <optionhomes11@penguin.lxd>
2020-12-13 19:31:26 -0500

    jj

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -5,7 +5,7 @@
 const https = require("https");
 
 // const fetch = request('node-fetch').default;
-const fetch = require('node-fetch').default
+// const fetch = require('node-fetch').default
 
 console.log('to here');
 const { LIST } = process.env;
