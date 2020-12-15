83aa6f1fa82a572fe89067887d87c3d34974f855
optionhomes11 <optionhomes11@penguin.lxd>
2020-12-14 21:53:03 -0500

    jj

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,7 +2,7 @@
 
     require('dotenv').config();
     const fetch = require('node-fetch');
-    const https = require("https");
+    // const https = require("https");
 
     // const fetch = request('node-fetch').default;
     // const fetch = require('node-fetch').default
@@ -14,11 +14,17 @@
     // 1:52:15 PM: 2020-12-05T18:52:15.872Z	2107bd44-ece9-495a-afd7-b66502641175	INFO	undefined
     // 1:52:15 PM: Duration: 3.30 ms	Memory Usage: 67 MB	Init Duration: 164.43 ms	
     // 
+
+        // 'lists[]': '175546'
+          // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjU4MTA5OWQ5NjI0MzU4YjMxNGQxOWI1MmE5NDJkMWZiM2I0NjQzODZkYmMzMGYwMzY2MmZjMDEzYjJjZDI1OGYyMWU5YjZkYmI4ZTllNzJmIn0.eyJhdWQiOiI0IiwianRpIjoiNTgxMDk5ZDk2MjQzNThiMzE0ZDE5YjUyYTk0MmQxZmIzYjQ2NDM4NmRiYzMwZjAzNjYyZmMwMTNiMmNkMjU4ZjIxZTliNmRiYjhlOWU3MmYiLCJpYXQiOjE2MDI3MjAzMzUsIm5iZiI6MTYwMjcyMDMzNSwiZXhwIjoxNjM0MjU2MzM1LCJzdWIiOiIzNDE1NSIsInNjb3BlcyI6W119.aPIpeO8toorud_EIGlvHdIqANIxblv7Pjhc8HyNldZgki-aCCDXUBSvrrsVR5bpP_XIAvw8n1M1loDT8Ag2JissJ5knFD-Jn1yKUzkL0SnkA6kymhFppo0ueO5kH7LUElBZWOUcqBmJse8uyQ1eEDDQvurxoKpcfLKgxXWUcFGdnqaTRGTnBW1oilG132LLH4N2rnZzKa27gNPj0cD2jSfaQuDGeZxyCgwl7rp2YKLjKcRR1lMA1fs4SCdbrulZeavr_KxcSMJvxDw-jAcsnQIGMUBgw_7Tekn3knJ5qctTdWB_yeoEXxsNpL2nQTy-Q_5n9oS4t6mHkNCEy4HQ6CQlgjqrDUWRzAgUGZ0bw9ngtGVuGo1H8J9_5YqET6REoX34E04vJHZgpdOy5IAC3fKaRA2aUBn0uUwuvHVFkSMr4gGPr2QGHdIyGDM5LFu9ypF9cgRWHI42BqhNEsL9xGk9epoMmku3wACnBLXxaN5v2MV6oaIVPq_rsdmtAI_oB2_5PbbjaaAY5rj6y5garzRxm7Svlv0OwsMWU6BlCrZO-v2bHGb3fdVcB__uJ8_19qvoH03ehY-EkKwRQg65EOSn6SqL624mIO38t-K7YLRso-S9_f9gSCkVyy9nVTS6uZEN3hHvRYhG7zWTTPiCaBDkxbfg4xDlgiQnsICvJMos',
+
     exports.handler = async event => {
 
       // EMAIL, ASKING
 
       console.log('hi');
+      console.log(LIST);
+      console.log(B);
       // const email = 'bill66666@mailinator.com';
       const email = JSON.parse(event.body).payload.data.EMAIL
 
@@ -41,40 +47,35 @@
       var endpoint = 'https://api.sendfox.com/contacts/?' + encoded;
 
       const data = JSON.stringify(formData);
-      const options = {
+      // const options = {
 
         // hostname: 'sendfox.com',
-        method: 'POST',
-        connection: 'keep-alive',
+      // //   method: 'POST',
+      // //   connection: 'keep-alive',
 
-        headers: {
-          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjU4MTA5OWQ5NjI0MzU4YjMxNGQxOWI1MmE5NDJkMWZiM2I0NjQzODZkYmMzMGYwMzY2MmZjMDEzYjJjZDI1OGYyMWU5YjZkYmI4ZTllNzJmIn0.eyJhdWQiOiI0IiwianRpIjoiNTgxMDk5ZDk2MjQzNThiMzE0ZDE5YjUyYTk0MmQxZmIzYjQ2NDM4NmRiYzMwZjAzNjYyZmMwMTNiMmNkMjU4ZjIxZTliNmRiYjhlOWU3MmYiLCJpYXQiOjE2MDI3MjAzMzUsIm5iZiI6MTYwMjcyMDMzNSwiZXhwIjoxNjM0MjU2MzM1LCJzdWIiOiIzNDE1NSIsInNjb3BlcyI6W119.aPIpeO8toorud_EIGlvHdIqANIxblv7Pjhc8HyNldZgki-aCCDXUBSvrrsVR5bpP_XIAvw8n1M1loDT8Ag2JissJ5knFD-Jn1yKUzkL0SnkA6kymhFppo0ueO5kH7LUElBZWOUcqBmJse8uyQ1eEDDQvurxoKpcfLKgxXWUcFGdnqaTRGTnBW1oilG132LLH4N2rnZzKa27gNPj0cD2jSfaQuDGeZxyCgwl7rp2YKLjKcRR1lMA1fs4SCdbrulZeavr_KxcSMJvxDw-jAcsnQIGMUBgw_7Tekn3knJ5qctTdWB_yeoEXxsNpL2nQTy-Q_5n9oS4t6mHkNCEy4HQ6CQlgjqrDUWRzAgUGZ0bw9ngtGVuGo1H8J9_5YqET6REoX34E04vJHZgpdOy5IAC3fKaRA2aUBn0uUwuvHVFkSMr4gGPr2QGHdIyGDM5LFu9ypF9cgRWHI42BqhNEsL9xGk9epoMmku3wACnBLXxaN5v2MV6oaIVPq_rsdmtAI_oB2_5PbbjaaAY5rj6y5garzRxm7Svlv0OwsMWU6BlCrZO-v2bHGb3fdVcB__uJ8_19qvoH03ehY-EkKwRQg65EOSn6SqL624mIO38t-K7YLRso-S9_f9gSCkVyy9nVTS6uZEN3hHvRYhG7zWTTPiCaBDkxbfg4xDlgiQnsICvJMos',
-          // 'Authorization': B,
-          'Content-Type': 'application/json',
-        },
-        'content-length': data.length,
+      // //   headers: {
+      // //     'Authorization': B,
+      // //     'Content-Type': 'application/json',
+      // //   },
+      // //   'content-length': data.length,
 
-      };
-      console.log(email);
-
-      const req = https.request(endpoint, options, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-
-        res.on('data', (d) => {
-          console.log(d);
-        });
-      });
-
-      req.on('error', (e) => {
-        console.error(e);
-      });
-
-      req.write(data);
-      req.end();
-
-      return {
-        statusCode: 200,
-        body: data
-      };
+      // // };
+      // // const { EMAIL_TOKEN } = process.env
+      // exports.handler = async event => {
+        // const email = JSON.parse(event.body).payload.email
+        // console.log(`Recieved a submission: ${email}`)
+        return fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            Authorization: B,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ formData }),
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log(`Submitted to Buttondown:\n ${data}`)
+          })
+          .catch(error => ({ statusCode: 422, body: String(error) }))
+      // } 
     }
